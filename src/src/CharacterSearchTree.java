d072fe2b15a20d7190e6ac31b2bc13bfa8ad7d68
maks0bs <maksthebro@173gmail.com>
2019-12-12 22:38:21 +0100

    + Task 3 - 10, Task 11 in work

--- src/src/CharacterSearchTree.java
+++ src/src/CharacterSearchTree.java
@@ -2,6 +2,35 @@ public class CharacterSearchTree {
     private HuffmanTriple content;
     private CharacterSearchTree leftChild, rightChild;
 
+    public static class HelperClass{
+        public static int max(int a, int b){
+            if (a > b){
+                return a;
+            }
+            else{
+                return b;
+            }
+        }
+
+        public static int min(int a, int b){
+            if (a < b){
+                return a;
+            }
+            else{
+                return b;
+            }
+        }
+
+        public static char min(char a, char b){
+            if (a < b){
+                return a;
+            }
+            else{
+                return b;
+            }
+        }
+    }
+
     public CharacterSearchTree() {
         content = null;
         leftChild = null;
@@ -22,6 +51,7 @@ public class CharacterSearchTree {
         }
     }
 
+    //TODO fix bug in task 2: not clear if you have to set code for all entries of equal characters or create a seperate node for the same character, but with a different code
     public void add(char t, int q, String c){ //AUFGABE 2
         if ( isEmpty() ) {
             content = new HuffmanTriple(t, q);
@@ -56,6 +86,123 @@ public class CharacterSearchTree {
             rightChild.showPreOrder();
         }
     }
+
+    public int height(){//AUFGABE 4
+        if (isEmpty()){
+            return 0;
+        }
+        else{
+            return 1 + HelperClass.max(leftChild.height(), rightChild.height());
+        }
+    }
+
+    public int countCharacters(){ //AUFGABE 5
+        if (isEmpty()){
+            return 0;
+        }
+        else{
+            return this.content.getQuantity() + leftChild.countCharacters() + rightChild.countCharacters();
+        }
+    }
+
+    public int longestCode(){ //AUFGABE 6
+        if (isEmpty()){
+            return 0;
+        }
+        else{
+            String curCode = this.content.getCode();
+            String leftCode = this.leftChild.content.getCode();
+            String rightCode = this.rightChild.content.getCode();
+            int maxChildrenCodeLength = HelperClass.max(leftCode.length(), rightCode.length());
+            return HelperClass.max(maxChildrenCodeLength, curCode.length());
+        }
+    }
+
+    //Don't know how to solve without toArray yet
+    public HuffmanTriple minimum(){ //AUFGABE 7
+        if (isEmpty()){
+            return null;
+        }
+        else{
+            HuffmanTriple nodes[] = this.toArray();
+            char minChar = nodes[0].getToken();
+            HuffmanTriple result = nodes[0];
+
+
+            for (int i = 0; i < nodes.length; i++){
+                if (nodes[i].getToken() < minChar){
+                    minChar = nodes[i].getToken();
+                    result = nodes[i];
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public boolean hasOnlyCompleteNode(){ //AUFGABE 8 (not sure, but works correctly
+        if (isEmpty()){
+            return true;
+        }
+        else{
+            int childCount = 0;
+            if (!this.leftChild.isEmpty()){
+                childCount++;
+            }
+            if (!this.rightChild.isEmpty()){
+                childCount++;
+            }
+
+            if (childCount == 1 || !this.leftChild.hasOnlyCompleteNode() || !this.rightChild.hasOnlyCompleteNode()){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+
+    public boolean containsCharacter(char t){ //AUFGABE 9
+        if (isEmpty()){
+            return false;
+        }
+        else{
+            if (this.content.getToken() == t ||
+                this.leftChild.containsCharacter(t) || this.rightChild.containsCharacter(t)){
+
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    public boolean equalStructure(CharacterSearchTree cst){ //AUFGABE 10
+        if (this.isEmpty() && cst.isEmpty()){
+            return true;
+        }
+        else if ((this.isEmpty() && !cst.isEmpty()) || (!this.isEmpty() && cst.isEmpty())){
+            return false;
+        }
+        boolean equalLeft = this.leftChild.equalStructure(cst.leftChild);
+        boolean equalRight = this.rightChild.equalStructure(cst.rightChild);
+        return !cst.isEmpty() && !this.isEmpty() && equalLeft && equalRight;
+    }
+
+
+    /*  not sure how to do this without changing the structure - only the case,
+        described in the example is possible to rotate in such a way,
+        otherwise the right child of the root's left child has to move to the right subtree
+        see https://en.wikipedia.org/wiki/Tree_rotation
+     */
+    public CharacterSearchTree rotateNodeToRight(){ //AUFGABE 11
+        CharacterSearchTree rightChildOfLeft =
+
+
+    }
+
+
 
     public boolean isEmpty() {
         return content == null;
--- src/src/ConsoleTest.java
+++ src/src/ConsoleTest.java
@@ -1,7 +1,12 @@
 public class ConsoleTest {
+    public static final String testingString = "4261357";
 
     public static void main(String args[]){
-        CharacterSearchTree tree = new CharacterSearchTree("testingtree".toCharArray());
+        CharacterSearchTree tree = new CharacterSearchTree(testingString.toCharArray());
         tree.showPreOrder();
+        System.out.println(tree.height());
+        System.out.println(tree.countCharacters());
+        //tree.add('z', 5, "testcode");
+        System.out.println(tree.equalStructure(new CharacterSearchTree("5372460".toCharArray())));
     }
 }
