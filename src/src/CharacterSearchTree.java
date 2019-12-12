eacc10d40723ce12a9263f083337de162a323d95
maks0bs <maksthebro@173gmail.com>
2019-12-12 19:59:31 +0100

    Tasks 1 and 2

--- src/src/CharacterSearchTree.java
+++ src/src/CharacterSearchTree.java
@@ -1,88 +1,99 @@
-public class CharacterSearchTree
-{
+public class CharacterSearchTree {
     private HuffmanTriple content;
     private CharacterSearchTree leftChild, rightChild;
 
-    public CharacterSearchTree()
-    {
+    public CharacterSearchTree() {
         content = null;
         leftChild = null;
         rightChild = null;
     }
 
-    public HuffmanTriple getContent()
-    {
-        if ( !isEmpty() )
-        {
+    public HuffmanTriple getContent() {
+        if ( !isEmpty() ) {
             return content;
         } else {
             throw new IllegalStateException();
         }
     }
 
-    public boolean isEmpty()
-    {
+    public CharacterSearchTree(char str[]){ //AUFGABE 1
+        for (int i = 0; i < str.length; i++){
+            this.add(str[i]);
+        }
+    }
+
+    public void add(char t, int q, String c){ //AUFGABE 2
+        if ( isEmpty() ) {
+            content = new HuffmanTriple(t, q);
+            content.setCode(c);
+            leftChild = new CharacterSearchTree();
+            rightChild = new CharacterSearchTree();
+        }
+        else {
+            if ( content.getToken() > t ) {
+                leftChild.add(t, q, c);
+            }
+            else if ( content.getToken() < t ) {
+                rightChild.add(t, q, c);
+            }
+            else {
+                for (int i = 0; i < q; i++){
+                    content.incrementQuantity();
+                }
+
+                content.setCode(c);
+            }
+        }
+    }
+
+    public boolean isEmpty() {
         return content == null;
     }
 
-    public boolean isLeaf()
-    {
+    public boolean isLeaf() {
         return !isEmpty() && leftChild.isEmpty() && rightChild.isEmpty();
     }
 
-    public void add( char t )
-    {
-        if ( isEmpty() )
-        {
+    public void add( char t ) {
+        if ( isEmpty() ) {
             content = new HuffmanTriple( t );
             leftChild = new CharacterSearchTree();
             rightChild = new CharacterSearchTree();
         }
-        else
-        {
-            if ( content.getToken() > t )
-            {
+        else {
+            if ( content.getToken() > t ) {
                 leftChild.add( t );
             }
-            else if ( content.getToken() < t )
-            {
+            else if ( content.getToken() < t ) {
                 rightChild.add( t );
             }
-            else
-            {
+            else {
                 content.incrementQuantity();
             }
         }
     }
 
-    public void iterativeAdd( char t )
-    {
+    public void iterativeAdd( char t ) { //Why do we need this method if we have add?
         CharacterSearchTree current = this;
-        while ( !current.isEmpty() && current.content.getToken() != t )
-        {
-            if ( current.content.getToken() > t )
-            {
+        while ( !current.isEmpty() && current.content.getToken() != t ) {
+            if ( current.content.getToken() > t ) {
                 current = current.leftChild;
             }
-            else
-            {
+            else {
                 current = current.rightChild;
             }
         }
-        if ( current.isEmpty() )
-        {
+        if ( current.isEmpty() ) {
             current.content = new HuffmanTriple( t );
             current.leftChild = new CharacterSearchTree();
             current.rightChild = new CharacterSearchTree();
         }
-        else
-        {
+        else {
             current.content.incrementQuantity();
         }
     }
 
-    public String getCode( char t )
-    {
+    public String getCode( char t ) {
         if ( !isEmpty() )
         {
             if ( content.getToken() > t )
@@ -104,8 +115,7 @@ public class CharacterSearchTree
         }
     }
 
-    public int size()
-    {
+    public int size() {
         if ( isEmpty() )
         {
             return 0;
@@ -116,8 +126,7 @@ public class CharacterSearchTree
         }
     }
 
-    public void show()
-    {
+    public void show() {
         if ( !isEmpty() )
         {
             leftChild.show();
@@ -126,8 +135,7 @@ public class CharacterSearchTree
         }
     }
 
-    public HuffmanTriple[] toArray()
-    {
+    public HuffmanTriple[] toArray() {
         if ( !isEmpty() )
         {
             HuffmanTriple[] collector = new HuffmanTriple[size()];
@@ -137,8 +145,7 @@ public class CharacterSearchTree
         return new HuffmanTriple[0];
     }
 
-    private int toArray( HuffmanTriple[] collector, int index )
-    {
+    private int toArray( HuffmanTriple[] collector, int index ) {
         if ( !isEmpty() )
         {
             index = leftChild.toArray( collector, index );
